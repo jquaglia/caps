480353f4119b8733da56b7d98d9e8315b57f9a67
jquaglia <jqugali@gmail.com>
2021-03-03 21:49:28 -0800

    finished lab

--- server/server.js
+++ server/server.js
@@ -5,6 +5,25 @@ const socketio = require('socket.io');
 const io = socketio(3000);
 const caps = io.of('/caps');
 
+const packageQueue = {
+  flowershop: {
+    pickup: {},
+    transit: {},
+    delivered: {},
+  },
+  acme: {
+    pickup: {},
+    transit: {},
+    delivered: {},
+  },
+};
+
+// const packageQueue = {
+//   pickup: {},
+//   transit: {},
+//   delivered: {},
+// };
+
 io.on('connection', socket => {
 
   console.log('New connection created : ' + socket.id);
@@ -17,17 +36,50 @@ caps.on('connection', capsSocket => {
 
   capsSocket.on('pickup', payload => {
     logger('pickup', payload);
+    if (payload.payload.store === '1-206-flowers') {
+      packageQueue.flowershop.pickup[payload.payload.orderID] = payload;
+    } else {
+      packageQueue.acme.pickup[payload.payload.orderID] = payload;
+    }
+    // packageQueue.pickup[payload.payload.orderID] = payload;
     capsSocket.broadcast.emit('pickup', payload);
   });
 
   capsSocket.on('in-transit', payload => {
     logger('in-transit', payload);
+    if (payload.payload.store === '1-206-flowers') {
+      delete packageQueue.flowershop.pickup[payload.payload.orderID];
+      packageQueue.flowershop.transit[payload.payload.orderID] = payload;
+    } else {
+      delete packageQueue.acme.pickup[payload.payload.orderID];
+      packageQueue.acme.transit[payload.payload.orderID] = payload;
+    }
+    // delete packageQueue.pickup[payload.payload.orderID];
+    // packageQueue.transit[payload.payload.orderID] = payload; 
     capsSocket.broadcast.emit('in-transit', payload);
   });
 
   capsSocket.on('delivered', payload => {
     logger('delivered', payload);
+    if (payload.payload.store === '1-206-flowers') {
+      delete packageQueue.flowershop.transit[payload.payload.orderID];
+      packageQueue.flowershop.delivered[payload.payload.orderID] = payload;
+    } else {
+      delete packageQueue.acme.transit[payload.payload.orderID];
+      packageQueue.acme.delivered[payload.payload.orderID] = payload;
+    }
+    // delete packageQueue.transit[payload.payload.orderID];
+    // packageQueue.delivered[payload.payload.orderID] = payload;
     capsSocket.broadcast.emit('delivered', payload);
+  });
+
+  capsSocket.on('getAll', () => {
+    for (let key in packageQueue.pickup) {
+      capsSocket.emit('pickup', packageQueue.flowershop.pickup[key]);
+    }
+    for (let key in packageQueue.acme.pickup) {
+      capsSocket.emit('pickup', packageQueue.acme.pickup[key]);
+    }
   });
 });
 
